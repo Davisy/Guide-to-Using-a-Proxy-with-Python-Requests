9ca48a503a44b0dc9d230b29e39681054691064b
Davis David <davisdavid179@gmail.com>
2023-05-25 13:04:27 +0300

    Create How to Set Proxies Directly in Requests.py

---
+++ How to Set Proxies Directly in Requests.py
@@ -0,0 +1,25 @@
+# import packages.  
+import requests  
+from bs4 import BeautifulSoup  
+  
+# Define proxies to use.  
+proxies = {  
+    'http': ='http://proxyprovider.com:2000',  
+    'https': 'http://proxyprovider.com:2000',  
+}  
+  
+# Define a link to the web page.  
+url = "https://brightdata.com/"  
+  
+# Send a GET request to the website.  
+response = requests.get(url, proxies=proxies)  
+  
+# Use BeautifulSoup to parse the HTML content of the website.  
+soup = BeautifulSoup(response.content, "html.parser")  
+  
+# Find all the links on the website.  
+links = soup.find_all("a")  
+  
+# Print all the links.  
+for link in links:  
+    print(link.get("href"))
